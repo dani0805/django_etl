b78c2685396943e5d32a3f0ddf3b1ef1188cfe1f
Daniele Bernardini <daniele.bernardini@intranetstandard.biz>
2019-03-05 13:57:33 +0100

    refactored task execution

--- etl/worker.py
+++ etl/worker.py
@@ -6,7 +6,7 @@ import sqlite3
 
 from django.utils.timezone import now
 
-from etl.models import Job, Database, JobStatus, TaskStatus
+from etl.models import Job, Database, JobStatus, TaskStatus, Task
 
 
 class Worker:
@@ -35,48 +35,52 @@ class Worker:
 
         # loop through task
         for task in self.job.task_set.filter(active=True):
-            if TaskStatus.objects.filter(job=self.job, task=task, batch_id=batch_id).exists():
-                return 0
-            else:
-                TaskStatus.objects.create(job=self.job, task=task, batch_id=batch_id, started_on=now(), status="running")
-
-            s_cursor = source_connection.cursor()
-            t_cursor = target_connection.cursor()
-            # truncate target if required
-            if task.truncate_on_load:
-                tr_cursor = target_connection.cursor()
-                if self.job.destination.type == "sqlite":
-                    tr_cursor.execute("delete from {}".format(task.destination_table))
-                else:
-                    tr_cursor.execute("truncate table {}".format(task.destination_table))
-                tr_cursor.close()
-            target_connection.commit()
-            # while there are chunks left
-            #print(task.extract_query(batch_id=batch_id))
-            #s_cursor.execute("select * from A")
-            #print(s_cursor.fetchall())
-            s_cursor.execute(task.extract_query(batch_id=batch_id))
-            # select next chunk to memory
-            data = s_cursor.fetchmany(size=task.chunk_size) if task.chunk_size > 0 else s_cursor.fetchall()
-            #print(data)
-            while data:
-                # write chunk to destination
-                #print(task.load_query(batch_id=batch_id))
-                #print(data)
-                query = task.load_query(batch_id=batch_id)
-                #print(query)
-                t_cursor.executemany(query, data)
-                target_connection.commit()
-                data = s_cursor.fetchmany(size=task.chunk_size) if task.chunk_size > 0 else None
-            s_cursor.close()
-            t_cursor.close()
-            TaskStatus.objects.filter(job=self.job, task=task, batch_id=batch_id, status="running").update(completed_on=now(),
-                status="completed")
+            self.run_task(task=task, target_connection=target_connection, source_connection=source_connection, batch_id=batch_id)
 
         # log batch completed
         JobStatus.objects.filter(job=self.job, batch_id=batch_id, status="running").update(completed_on=now(), status="completed")
 
         return 1
+
+    def run_task(self, *, task: Task, source_connection, target_connection, batch_id: str):
+        # log target start
+        if TaskStatus.objects.filter(job=self.job, task=task, batch_id=batch_id).exists():
+            return 0
+        else:
+            TaskStatus.objects.create(job=self.job, task=task, batch_id=batch_id, started_on=now(), status="running")
+
+        s_cursor = source_connection.cursor()
+        t_cursor = target_connection.cursor()
+        # truncate target if required
+        self.truncate_target_table(target_connection=target_connection, task=task)
+        target_connection.commit()
+        s_cursor.execute(task.extract_query(batch_id=batch_id))
+        # select next chunk to memory
+        data = s_cursor.fetchmany(size=task.chunk_size) if task.chunk_size > 0 else s_cursor.fetchall()
+        # while there are chunks left
+        while data:
+            # write chunk to destination
+            query = task.load_query(batch_id=batch_id)
+            t_cursor.executemany(query, data)
+            target_connection.commit()
+            # select next chunk to memory
+            data = s_cursor.fetchmany(size=task.chunk_size) if task.chunk_size > 0 else None
+        s_cursor.close()
+        t_cursor.close()
+        # log target end
+        TaskStatus.objects.filter(job=self.job, task=task, batch_id=batch_id, status="running").update(
+            completed_on=now(),
+            status="completed")
+
+
+    def truncate_target_table(self, *, target_connection, task):
+        if task.truncate_on_load:
+            tr_cursor = target_connection.cursor()
+            if self.job.destination.type == "sqlite":
+                tr_cursor.execute("delete from {}".format(task.destination_table))
+            else:
+                tr_cursor.execute("truncate table {}".format(task.destination_table))
+            tr_cursor.close()
 
     def connect(self, *, db: Database):
         connect_string = json.loads(db.connection_string)
